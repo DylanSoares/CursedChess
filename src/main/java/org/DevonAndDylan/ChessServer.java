2fb72e3fad656e33462c602418a9c153b8d4af57
DylanSoares <dylan.soares@myokanagan.bc.ca>
2023-03-28 16:33:25 -0700

    Added simple communication example

--- src/main/java/org/DevonAndDylan/ChessServer.java
+++ src/main/java/org/DevonAndDylan/ChessServer.java
@@ -1,4 +1,25 @@
 package org.DevonAndDylan;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ChessServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(7777);
+
+        //waits for connection
+
+        Socket clientSocket = serverSocket.accept(); // this blocks so no other client can connect now.
+        //itll need to be threaded and somehow we will have to make these communicate.
+
+        InputStream inputStream = clientSocket.getInputStream();
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+        System.out.println(dataInputStream.readUTF());
+        serverSocket.close();
+        clientSocket.close();
+    }
 }
