a69135a29b3c544302bf11c516cc37217544287c
DylanSoares <dylan.soares@myokanagan.bc.ca>
2023-03-28 16:28:36 -0700

    Added Classes

--- src/main/java/org/DevonAndDylan/ChessClient.java
+++ src/main/java/org/DevonAndDylan/ChessClient.java
@@ -1,5 +1,29 @@
 package org.DevonAndDylan;
 
+import org.DevonAndDylan.Pieces.*;
+import java.util.ArrayList;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class ChessClient {
 
+    public static void main(String[] args) throws IOException {
+
+        //just gonna put this here
+        String move = "e4 Nc3";
+
+        Socket socket = new Socket("localhost", 7777);
+        System.out.println("DEBUG: Connected to server.");
+
+        OutputStream outputStream = socket.getOutputStream(); // get the output stream from the socket.
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+        // write the message we want to send
+        dataOutputStream.writeUTF(move);
+        dataOutputStream.flush(); // send the message
+        dataOutputStream.close(); // close the output stream when we're done.
+        socket.close();
+    }
 }
