01fb5d2b96710b8dee2d22a6eb695e7c2cb3ab4e
AwesomeAim <30537683+AwesomeAim@users.noreply.github.com>
2023-04-04 16:24:16 -0700

    shorten populate code

--- src/main/java/org/DevonAndDylan/Pieces/Board.java
+++ src/main/java/org/DevonAndDylan/Pieces/Board.java
@@ -24,22 +24,18 @@ public class Board {
 		this.pieces = pieces;
 	}
 	private void populate() {
-		pieces.add(new Rook(new Location('a', 1), true));
-		pieces.add(new Rook(new Location('h', 1), true));
-		pieces.add(new Knight(new Location('b', 1), true));
-		pieces.add(new Knight(new Location('g', 1), true));
-		pieces.add(new Bishop(new Location('c', 1), true));
-		pieces.add(new Bishop(new Location('f', 1), true));
-		pieces.add(new Queen(new Location('d', 1), true));
-		pieces.add(new King(new Location('e', 1), true));
-		pieces.add(new Rook(new Location('a', 8), false));
-		pieces.add(new Rook(new Location('h', 8), false));
-		pieces.add(new Knight(new Location('b', 8), false));
-		pieces.add(new Knight(new Location('g', 8), false));
-		pieces.add(new Bishop(new Location('c', 8), false));
-		pieces.add(new Bishop(new Location('f', 8), false));
-		pieces.add(new Queen(new Location('d', 8), false));
-		pieces.add(new King(new Location('e', 8), true));
+		boolean white = true;
+		for (int i=1;i<=width;i+=7) {
+			pieces.add(new Rook(new Location('a', i), white));
+			pieces.add(new Rook(new Location('h', i), white));
+			pieces.add(new Knight(new Location('b', i), white));
+			pieces.add(new Knight(new Location('g', i), white));
+			pieces.add(new Bishop(new Location('c', i), white));
+			pieces.add(new Bishop(new Location('f', i), white));
+			pieces.add(new Queen(new Location('d', i), white));
+			pieces.add(new King(new Location('e', i), white));
+			white = false;
+		}
 		
 		for (int i=1;i<=width;i++) {
 			pieces.add(new Pawn(new Location(toChar(i), 2), true));
