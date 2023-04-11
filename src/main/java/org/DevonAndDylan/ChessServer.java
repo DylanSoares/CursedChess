7305afc44a0d0766c82a36dac6b3a0eb0245d722
DylanSoares <dylan.soares@myokanagan.bc.ca>
2023-04-11 15:37:11 -0700

    Initial server code (Promotions not handled)

--- src/main/java/org/DevonAndDylan/ChessServer.java
+++ src/main/java/org/DevonAndDylan/ChessServer.java
@@ -1,6 +1,7 @@
 package org.DevonAndDylan;
 
 import org.DevonAndDylan.Pieces.Board;
+import org.DevonAndDylan.Pieces.Location;
 import org.DevonAndDylan.Pieces.Piece;
 
 import java.io.*;
@@ -23,13 +24,17 @@ public class ChessServer {
         System.out.println("Client 2 connected");
 
         // Create input and output streams for each client
-        ObjectInputStream in1 = new ObjectInputStream(client1.getInputStream());
+
         ObjectOutputStream out1 = new ObjectOutputStream(client1.getOutputStream());
-        ObjectInputStream in2 = new ObjectInputStream(client2.getInputStream());
+
         ObjectOutputStream out2 = new ObjectOutputStream(client2.getOutputStream());
 
-//        out1.writeObject((Boolean)false); // player one is white
-//        out2.writeObject((Boolean)true); // player two is black
+        ObjectInputStream in1 = new ObjectInputStream(client1.getInputStream());
+        ObjectInputStream in2 = new ObjectInputStream(client2.getInputStream());
+
+        //playersChoice
+        out1.writeObject(false); // player one is white
+        out2.writeObject(true); // player two is black
 
 
         // Initially deliver the board to clients
@@ -37,25 +42,66 @@ public class ChessServer {
         out1.writeObject(pieces);
         out2.writeObject(pieces);
 
-        // Wait for messages from the clients and forward them to the other client
+//         Wait for messages from the clients and forward them to the other client
         while (true) {
             // Write pieces from board to the client
             boolean isWhiteTurn = board.getWhoseTurn();
             boolean promotionNeeded = board.isPromote();
 
             pieces = board.toPieceArray();
+            //Pieces
             out1.writeObject(pieces);
             out2.writeObject(pieces);
+            //Promotion
             out1.writeObject(promotionNeeded);
             out2.writeObject(promotionNeeded);
+            //isWhiteTurn
             out1.writeObject(isWhiteTurn);
             out2.writeObject(isWhiteTurn);
-            out1.writeObject(board.toPieceArray());
-            out2.writeObject(board.toPieceArray());
 
-            out1.flush();
-            out2.flush();
 
+            if (isWhiteTurn) {
+                out1.writeObject(true);
+                out2.writeObject(false);
+                if (promotionNeeded) {
+
+                } else {
+                    char[] moveCommand = (char[]) in1.readObject();
+
+                    char x1 = Board.toChar(Integer.parseInt(String.valueOf(moveCommand[0])) + 1);
+                    char x2 = Board.toChar(Integer.parseInt(String.valueOf(moveCommand[2])) + 1);
+                    int y1 = Integer.parseInt(String.valueOf(moveCommand[1])) + 1;
+                    int y2 = Integer.parseInt(String.valueOf(moveCommand[3])) + 1;
+
+                    int result = board.move(new Location(x1, y1), new Location(x2, y2));
+                    System.err.println("Result of move in1: " + result);
+                    out1.writeObject(result);
+                }
+                out2.writeObject("Other player has finished their turn.");
+            } else {
+                out1.writeObject(false);
+                out2.writeObject(true);
+                if (promotionNeeded) {
+                    //TODO implement client and server promotion
+                } else {
+
+                    char[] moveCommand = (char[]) in2.readObject();
+
+                    char x1 = Board.toChar(Integer.parseInt(String.valueOf(moveCommand[0])) + 1);
+                    char x2 = Board.toChar(Integer.parseInt(String.valueOf(moveCommand[2])) + 1);
+                    int y1 = Integer.parseInt(String.valueOf(moveCommand[1])) + 1;
+                    int y2 = Integer.parseInt(String.valueOf(moveCommand[3])) + 1;
+
+                    int result = board.move(new Location(x1, y1), new Location(x2, y2));
+                    System.err.println("Result of move in2: " + result);
+                    out2.writeObject(result);
+                }
+                out1.writeObject("Other player has finished their turn.");
+            }
+            System.err.println("loop itr");
         }
+//        client1.close();
+//        client2.close();
+//        serverSocket.close();
     }
 }
